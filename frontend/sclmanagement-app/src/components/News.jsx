0cb3bebd763001974308de9b1037f3a2d6aec714
MD MAHFUJUR RAHMAN <mahfujur-22ia@dipti.com.bd>
2026-07-30 11:53:53 +0300

    Fix import path for fetchAllNotices function

--- frontend/sclmanagement-app/src/components/News.jsx
+++ frontend/sclmanagement-app/src/components/News.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { fetchAllNotices } from "../api/noticeApi";
+import { fetchAllNotices } from "../api/noticeapi";
 
 const News = () => {
   const [notices, setNotices] = useState([]);
